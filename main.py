d94f52b3fa483d642e00ed861af25dda037bfc8e
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-10-17 17:03:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
- 
-with open('text.txt', 'r', encoding='utf-8') as file:
-    spis = f.read()
-words=0
-for line in file:
-  words+=len(line.split())
-  print("кол-во слов в файле : ", words)
+text=open('text.txt', encoding="utf-8")   #открываем нужный файл
+print("Kol-vo slov : ", len(text.read().split())) #Вывод на экран текст, кол-во слов 
+text.close()#закрываем файл
