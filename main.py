912c084e045f5efae138920db5791ed591485006
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-10-20 12:51:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-text=open('text.txt', encoding="utf-8")   #открываем нужный файл
-print("Kol-vo slov : ", len(text.read().split())) #Вывод на экран текстa, кол-во слов 
-text.close()#закрываем файл
+text=open('text.txt', encoding="utf-8") # Открываем нужный файл
+print("Kol-vo slov : ", len(text.read().split())) # Вывод на экран текстa, кол-во слов 
+text.close() # Закрываем файл
